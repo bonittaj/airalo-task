8c39bbe0daf79735263ded266da55100e571196e
Bonitta <bonitta0712@gmail.com>
2025-05-23 18:52:46 +0200

    change config location

--- tests/api/api.spec.js
+++ tests/api/api.spec.js
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-import {generateAccessToken} from '../../config/global-setup'
+import {generateAccessToken} from '../../utilities/token'
 import dotenv from 'dotenv';
 dotenv.config();
 
